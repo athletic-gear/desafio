e3af5ad7009876fd1bec8d1ffed62b4364f3a51e
dev <adriana.bispo283@gmail.com>
2023-08-24 22:15:09 -0300

    feat: adcionado provider

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,19 +1,24 @@
+import 'package:desafio_athletic_gear/src/features/controller/password_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 //view
 import 'features/view/home_view.dart';
+//utils
+import 'shared/utils/app_theme.dart';
 
 class AthleticGear extends StatelessWidget {
   const AthleticGear({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Provider(
+      create: (_) => PasswordController(),
+      child: MaterialApp(
+        title: 'Athletic Gear',
+        theme: AppTheme.theme,
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
-
